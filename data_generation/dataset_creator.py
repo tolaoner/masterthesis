872e72df672552a3a19be6dae80e9d40e5475948
tolaoner <59030417+tolaoner@users.noreply.github.com>
2021-01-26 08:09:58 +0100

    extend the calculation for 5 voxels, create proper list/dataframe, create csv file for data storage

--- data_generation/dataset_creator.py
+++ data_generation/dataset_creator.py
@@ -3,18 +3,43 @@ from pulse_generator import generate_pulse
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+import pandas as pd
 from scipy.integrate import odeint
 t_span1 = np.linspace(0, 0.016, 160)
 t_span2 = np.linspace(0, 10, 20000)
 M_ic = [0, 0, 1]  # initial conditions
-b_x, b_y = generate_pulse()
-b_z = lambda a: 10 + 0 * math.sin(200 * math.pi * a)
-M = solve_bloch(t_span1, M_ic, b_x, b_y, b_z)
+b_z = [-20, 10, 0, 10, 20]
+features = ['M_ic', 'T', 'Mx_f', 'My_f', 'Mz_f', 'B_z']
+list_data = []
+t=0.016
+m_x = []
+m_y = []
+m_z = []
+for i in range(10):
+    for i in range(5):
+        b_x, b_y = generate_pulse()
+        M = solve_bloch(t_span1, M_ic, b_x, b_y, b_z[i])
+        m_x.append(M[-1, 0])
+        m_y.append(M[-1, 1])
+        m_z.append(M[-1, 2])
+    #print(m_x, '\n', m_y, '\n', m_z)
+    row_data=[M_ic, t, m_x, m_y, m_z, b_z]
+    list_data.append(row_data)
+    m_x=[]
+    m_y=[]
+    m_z=[]
+frame_data = pd.DataFrame(list_data, columns=features)
+#pd.set_option('display.max_columns',None)
+#print(frame_data.iloc[1]['Mx_f'])
+with open('generated_data.csv', 'a') as f:
+    frame_data.to_csv(f, mode='a', index=False, header=not f.tell())
+returned_data = pd.read_csv('generated_data.csv')
+print(returned_data.to_string())
 # plot magnetisation vs time
-plt.plot(t_span1, M[:, 0], 'b-', linewidth=2, label='M_x')
+'''plt.plot(t_span1, M[:, 0], 'b-', linewidth=2, label='M_x')
 plt.plot(t_span1, M[:, 1], 'r-', linewidth=2, label='M_y')
 plt.plot(t_span1, M[:, 2], 'g-', linewidth=2, label='M_z')
 plt.xlabel('time')
 plt.ylabel('m_x, m_y, m_z')
 plt.legend()
-plt.show()
+plt.show()'''
--- data_generation/pulse_generator.py
+++ data_generation/pulse_generator.py
@@ -7,12 +7,12 @@ def generate_pulse():
     f2 = 1
     f3 = 2
     a11, a22, a33, b11, b22, b33 = np.random.uniform(0, 1000, size=(6)) #random float values
-    a1, a2, a3, b1, b2, b3 = np.random.randint(0, 1000, size=(6)) # random int values
+    a1, a2, a3, b1, b2, b3 = np.random.randint(0, 10000, size=(6)) # random int values
     b_x = lambda t: a1*np.cos(f1*t)+a2*np.cos(f2*t)+a3*np.cos(f3*t)
     b_y = lambda t: b1*np.sin(f1*t)+b2*np.sin(f2*t)+b3*np.sin(f3*t)
     return b_x, b_y
-
-'''b_x, b_y = generate_pulse()
+'''for i in range(5):
+    b_x, b_y = generate_pulse()
 plt.plot(t_span, b_x(t_span), 'r-', linewidth= 2, label='b_x')
 plt.plot(t_span, b_y(t_span), 'b-', linewidth= 2, label='b_y')
 plt.legend()
