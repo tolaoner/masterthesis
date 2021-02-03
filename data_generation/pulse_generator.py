dadd41105982c8e0b9fd6fcdee88a9fa43dec71a
tolaoner <59030417+tolaoner@users.noreply.github.com>
2021-02-03 01:40:49 +0100

    make freqeuncy values random and change pulse format to 2step sinusoidal and cosinusoidal

--- data_generation/pulse_generator.py
+++ data_generation/pulse_generator.py
@@ -3,16 +3,14 @@ import matplotlib.pyplot as plt
 #t_span = np.linspace(0, 10, 1000)
 def generate_pulse():
     """generate random pulse in the form of sinusoids and cosinusoids"""
-    f1 = -3
-    f2 = 1
-    f3 = 2
+    f1, f2 = np.random.randint(-3, 4, size=(2))#frequency values
     a11, a22, a33, b11, b22, b33 = np.random.uniform(0, 1000, size=(6)) #random float values
-    a1, a2, a3, b1, b2, b3 = np.random.randint(0, 10000, size=(6)) # random int values
-    b_x = lambda t: a1*np.cos(f1*t)+a2*np.cos(f2*t)+a3*np.cos(f3*t)
-    b_y = lambda t: b1*np.sin(f1*t)+b2*np.sin(f2*t)+b3*np.sin(f3*t)
+    a1, a2, b1, b2 = np.random.randint(100, 1000, size=(4)) # random int values
+    b_x = lambda t: a1*np.cos(f1*t)+a2*np.cos(f2*t)
+    b_y = lambda t: b1*np.sin(f1*t)+b2*np.sin(f2*t)
     return b_x, b_y
-'''for i in range(5):
-    b_x, b_y = generate_pulse()
+'''t_span = np.linspace(0, 10, 160)
+b_x, b_y = generate_pulse()
 plt.plot(t_span, b_x(t_span), 'r-', linewidth= 2, label='b_x')
 plt.plot(t_span, b_y(t_span), 'b-', linewidth= 2, label='b_y')
 plt.legend()
