3f7735b8d889a1a6d4dc3cd0dd36a5f90d622dc0
tolaoner <59030417+tolaoner@users.noreply.github.com>
2021-02-03 01:57:37 +0100

    randomized excitation duration for each calculation

--- data_generation/dataset_creator.py
+++ data_generation/dataset_creator.py
@@ -5,20 +5,21 @@ import math
 import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.integrate import odeint
-t_span1 = np.linspace(0, 0.016, 160)
-t_span2 = np.linspace(0, 10, 20000)
+'''t_span1 = np.linspace(0, 0.016, 160)
+t_span2 = np.linspace(0, 10, 20000)'''
 M_ic = [0, 0, 1]  # initial conditions
 b_z = [-20, 10, 0, 10, 20]
 features = ['M_ic', 'T', 'Mx_f', 'My_f', 'Mz_f', 'B_z']
 list_data = []
-t=0.016
 m_x = []
 m_y = []
 m_z = []
 for i in range(10):
     for i in range(5):
+        t = np.random.randint(1, 16) / 100
+        t_span = np.linspace(0, t, 150)
         b_x, b_y = generate_pulse()
-        M = solve_bloch(t_span1, M_ic, b_x, b_y, b_z[i])
+        M = solve_bloch(t_span, M_ic, b_x, b_y, b_z[i])
         m_x.append(M[-1, 0])
         m_y.append(M[-1, 1])
         m_z.append(M[-1, 2])
@@ -34,7 +35,7 @@ frame_data = pd.DataFrame(list_data, columns=features)
 with open('generated_data.csv', 'a') as f:
     frame_data.to_csv(f, mode='a', index=False, header=not f.tell())
 returned_data = pd.read_csv('generated_data.csv')
-print(returned_data.to_string())
+#print(returned_data.to_string())
 # plot magnetisation vs time
 '''plt.plot(t_span1, M[:, 0], 'b-', linewidth=2, label='M_x')
 plt.plot(t_span1, M[:, 1], 'r-', linewidth=2, label='M_y')
